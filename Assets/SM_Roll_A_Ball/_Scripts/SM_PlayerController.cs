bbc80bbfe97c1323681e6efe268bbe51c0bbe21b
SPM170 <SPM170@CILABS-114B-13.scilsnet.rutgers.edu>
2018-05-31 19:48:31 -0400

    Roll A Ball Tutorial Video 7
    Added UI text and counter. Updated Player Controller

--- Assets/SM_Roll_A_Ball/_Scripts/SM_PlayerController.cs
+++ Assets/SM_Roll_A_Ball/_Scripts/SM_PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SM_PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -21,8 +28,6 @@ public class SM_PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
-
-
     }
 
     void OnTriggerEnter(Collider other)
@@ -30,6 +35,17 @@ public class SM_PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("SM Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
