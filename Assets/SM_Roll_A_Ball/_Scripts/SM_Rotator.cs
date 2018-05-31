2fabd34f5c3b38fc181df707f831dc2f349f37fc
SPM170 <SPM170@CILABS-114B-13.scilsnet.rutgers.edu>
2018-05-31 19:26:55 -0400

    Roll A Ball Tutorial Video 5
    Creating Collectible Items - Roll A Ball Tutorial 3.01

---
+++ Assets/SM_Roll_A_Ball/_Scripts/SM_Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SM_Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
